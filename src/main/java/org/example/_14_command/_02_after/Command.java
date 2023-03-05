eed2aad60aa9f5d422ca5501cbd380d55141f780
hyoseong Bae <bhsbhs235@naver.com>
2023-03-05 17:18:27 +0900

    command pattern

---
+++ src/main/java/org/example/_14_command/_02_after/Command.java
@@ -0,0 +1,9 @@
+package org.example._14_command._02_after;
+
+public interface Command {
+
+    void execute();
+
+    void undo();
+
+}
---
+++ src/main/java/org/example/_14_command/_02_after/MyApp.java
@@ -0,0 +1,21 @@
+package org.example._14_command._02_after;
+
+
+import org.example._14_command._01_before.Game;
+
+public class MyApp {
+
+    private Command command;
+
+    public MyApp(Command command) {
+        this.command = command;
+    }
+
+    public void press() {
+        command.execute();
+    }
+
+    public static void main(String[] args) {
+        MyApp myApp = new MyApp(new GameStartCommand(new Game()));
+    }
+}
